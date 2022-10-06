3d38b1d2f49b7a192bd626e0c485ee61d2f4c6df
naotomiura <85100588+naotomiura@users.noreply.github.com>
2022-10-06 15:50:31 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view(''posts/index'');
 });
 
 use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
