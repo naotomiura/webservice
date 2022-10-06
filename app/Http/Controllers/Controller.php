3753c5ca23b7667607c20d646f1ce44d0359caf3
naotomiura <85100588+naotomiura@users.noreply.github.com>
2022-10-06 12:00:26 +0900

    Postコントローラークラスの実装

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,3 +11,17 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
+
+//use宣言は外部にあるクラスをPostController内にインポートできる。
+//この場合、App\Models内のPostクラスをインポートしている。
+use App\Models\Post;
+/**
+ * Post一覧を表示する
+ * 
+ * @param Post Postモデル
+ * @return array Postモデルリスト
+ */
+public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+{
+    return $post->get();//$postの中身を戻り値にする。
+}
