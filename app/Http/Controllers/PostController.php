0e5a0e21f62d941fb7a88689544882729e2e785c
naotomiura <85100588+naotomiura@users.noreply.github.com>
2022-10-08 16:32:31 +0900

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest; // useする
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -24,6 +27,19 @@ class PostController extends Controller
     {
         $input = $request['post'];
         $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+    
         return redirect('/posts/' . $post->id);
     }
 }
